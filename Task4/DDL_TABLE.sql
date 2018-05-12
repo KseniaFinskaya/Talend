d18fe91ea0ef7f274f6a9b6d6e3fe7ba27b71303
Ksenia Finskaya <Ksenia_Finskaya@epam.com>
2018-05-13 00:29:09 +0300

    added task4

---
+++ Task4/DDL_TABLE.sql
@@ -0,0 +1,10 @@
+--------------------------------------------------------
+--  File created - Friday-May-11-2018   
+--------------------------------------------------------
+DROP TABLE DECODE_TABLE_BASE64;
+--------------------------------------------------------
+--  DDL for Table DECODE_TABLE_BASE64
+--------------------------------------------------------
+
+  CREATE TABLE DECODE_TABLE_BASE64 (encodedBytes BLOB,
+fileName varchar(300));
